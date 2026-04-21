0e40762e23d29a4254e0e596bccbe4c618454f19
ali-dev <aliawan1883@gmail.com>
2026-04-21 14:07:31 +0500

    Changed for branching feature

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello DevOps World'
+    return 'Hello DevOps World . This is a simple Flask application for my Devops Lab Mid!  '
 
 if __name__ == '__main__':
     app.run(debug=True)
